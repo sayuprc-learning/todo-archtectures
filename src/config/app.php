1c8be97edb62aa1b6368a3ed1976dc622d45a433
sayuprc <bepdielibe+github@gmail.com>
2024-12-24 19:24:25 +0900

    タイムゾーンを修正

--- src/config/app.php
+++ src/config/app.php
@@ -66,7 +66,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Tokyo'),
 
     /*
     |--------------------------------------------------------------------------
